6c5fe9e63bc5f89948c39b351107d634d50e300c
ESLAM <102685868+islam-abdel-halem@users.noreply.github.com>
2024-09-23 09:43:28 +0300

    add OOP files

--- h.kt
+++ h.kt
@@ -1,3 +1,27 @@
+class Persons {
+    var age: Int = 18
+        // Custom getter
+        get() {
+            println("Getting age value")
+            return field
+        }
+        // Custom setter
+        set(value) {
+            println("Setting age value to $value")
+            if (value > 0) {
+                field = value  // 'field' is a keyword used to refer to the backing field of the property
+            } else {
+                println("Age cannot be negative")
+            }
+        }
+}
+
 fun main() {
-    println("Hello Kotlin")
+    val person = Persons()
+
+    println(person.age)  // Calls the custom getter
+
+    person.age = 25  // Calls the custom setter
+
+    person.age = -5  // Setter prevents negative age
 }
